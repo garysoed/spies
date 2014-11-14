5c9d31957195a433af90a44f0dbe216e4c7ad5bf
Gary Soedarsono <garysoed@gmail.com>
2014-11-13 22:07:10 -0800

    fixed plugin

--- src/plugin/chai-expect.js
+++ src/plugin/chai-expect.js
@@ -2,11 +2,11 @@ chai.use(function(chai, utils) {
   utils.addMethod(chai.Assertion.prototype, 'calledWith', function() {
     var args = Array.prototype.slice.call(arguments, 0);
     var obj = utils.flag(this, 'object'); 
-    return expect(Spies.callCountOf(obj).apply(null, args));
+    return chai.expect(Spies.callCountOf(obj).apply(null, args));
   });
 
   utils.addMethod(chai.Assertion.prototype, 'called', function() {
     var obj = utils.flag(this, 'object'); 
-    return expect(Spies.anyCallCountOf(obj));
+    return chai.expect(Spies.anyCallCountOf(obj));
   });
 });
