c8a452322953fc436f37aac2a693ea69ca9da17e
Gary Soedarsono <garysoed@gmail.com>
2015-02-20 22:43:29 -0800

    doc

---
+++ doc/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Fakes",
+        "Matchers",
+        "SpiedFn",
+        "Spies",
+        "Utils",
+        "matcher.Any",
+        "matcher.IsA",
+        "matcher.Matcher"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
