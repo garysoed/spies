c547a72c775f89026d04a113f03ca076855df0fb
Gary Soedarsono <garysoed@gmail.com>
2015-02-22 23:06:26 -0800

    tutorials

--- api.js
+++ api.js
@@ -1,6 +1,11 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "1. Setup",
+        "2. Spies",
+        "3. Matchers",
+        "4. Fakes",
+        "5. Plugins",
         "Fakes",
         "Matchers",
         "SpiedFn",
@@ -10,7 +15,20 @@ YUI.add("yuidoc-meta", function(Y) {
         "matcher.IsA",
         "matcher.Matcher"
     ],
-    "modules": [],
-    "allModules": []
+    "modules": [
+        "api",
+        "tutorial"
+    ],
+    "allModules": [
+        {
+            "displayName": "api",
+            "name": "api"
+        },
+        {
+            "displayName": "tutorial",
+            "name": "tutorial",
+            "description": "There are several ways to use this library:\n- HTML Import `main.html`:\n```html\n<link rel=\"import\" href=\"path/to/main.html\">\n```\n- Or you can import the JS file directly:\n```html\n<script src=\"path/to/main.js\"></script>\n```\n- If you use the expect pattern of <a href=\"http://chaijs.com/\">Chai JS</a>, import\n  `main_chai_expect.html`, or import the plugin under `src/plugin/chai-expect.js`. If you load\n  the JS file, make sure to import the plugin after importing `chai.js`."
+        }
+    ]
 } };
 });
