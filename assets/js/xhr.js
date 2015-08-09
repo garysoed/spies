5e3c3d241b06bbdbde36500793ce81db722b68cc
Gary Soedarsono <garysoed@gmail.com>
2015-08-09 10:34:19 -0700

    updated

--- assets/js/xhr.js
+++ assets/js/xhr.js
@@ -6,11 +6,12 @@
   var sectionEls = document.querySelectorAll('.sidebar section');
   for (var i = 0; i < sectionEls.length; i++) {
     var sectionEl = sectionEls.item(i);
+    var sectionHeader = sectionEl.querySelector('header h2');
     var sidebarListItems = sectionEl.querySelectorAll('li');
     for (var j = 0; j < sidebarListItems.length; j++) {
       var sidebarListItem = sidebarListItems.item(j);
       var sidebarLink = sidebarListItem.querySelector('a');
-      sidebarLink.addEventListener('click', function(section, listItem, event) {
+      sidebarLink.addEventListener('click', function(section, sectionHeader, listItem, event) {
         // Update the history.
         history.pushState({}, '', this.href);
 
@@ -20,14 +21,26 @@
           selectedSection.classList.toggle('selected', false);
         }
 
-        var selectedLink = document.querySelector('.sidebar li.sidebar-selected');
-        if (selectedLink) {
-          selectedLink.classList.toggle('sidebar-selected', false);
+        var selectedListItem = document.querySelector('.sidebar li.sidebar-selected');
+        if (selectedListItem) {
+          selectedListItem.classList.toggle('sidebar-selected', false);
+        }
+
+        var primaryHeader = document.querySelector('.sidebar header h2.button.primary');
+        if (primaryHeader) {
+          primaryHeader.classList.toggle('primary', false);
+        }
+
+        var primaryLink = document.querySelector('.sidebar a.primary');
+        if (primaryLink) {
+          primaryLink.classList.toggle('primary', false);
         }
 
         // Selects the link
         listItem.classList.toggle('sidebar-selected', true);
         section.classList.toggle('selected', true);
+        sectionHeader.classList.toggle('primary', true);
+        this.classList.toggle('primary', true);
 
         // Grabs the page
         var xhr = new XMLHttpRequest();
@@ -50,7 +63,7 @@
         xhr.open('GET', this.href);
         xhr.send();
         event.preventDefault();
-      }.bind(sidebarLink, sectionEl, sidebarListItem));
+      }.bind(sidebarLink, sectionEl, sectionHeader, sidebarListItem));
     }
   }
 })(document, window);
